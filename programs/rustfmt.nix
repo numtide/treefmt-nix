9eb09799c6b231cd91b98058cff82ed2c862a5bf
Jörg Thalheim <joerg@thalheim.io>
2022-10-18 16:20:19 +0200

    rustfmt: bump edition and make configurable

--- programs/rustfmt.nix
+++ programs/rustfmt.nix
@@ -5,13 +5,20 @@ in
 {
   options.programs.rustfmt = {
     enable = lib.mkEnableOption "rustfmt";
+    edition = lib.mkOption {
+      type = lib.types.str;
+      default = "2021";
+      description = ''
+        Rust edition to target when formatting
+      '';
+    };
     package = lib.mkPackageOption pkgs "rustc" { };
   };
 
   config = lib.mkIf cfg.enable {
     settings.formatter.rustfmt = {
       command = "${cfg.package}/bin/rustfmt";
-      options = [ "--edition" "2018" ];
+      options = [ "--edition" cfg.edition ];
       includes = [ "*.rs" ];
     };
   };

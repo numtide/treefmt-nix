d325e0afad7848a58d13bbea2c856f1ad3dfe4a7
Hari Amoor <99296920+HariAmoor-professional@users.noreply.github.com>
2022-12-27 12:01:23 -0500

    Add stylish-haskell (#17)

---
+++ programs/stylish-haskell.nix
@@ -0,0 +1,18 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.programs.stylish-haskell;
+in
+{
+  options.programs.stylish-haskell = {
+    enable = lib.mkEnableOption "stylish-haskell";
+    package = lib.mkPackageOption pkgs "stylish-haskell" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.stylish-haskell = {
+      command = cfg.package;
+      options = [ "-i" "-r" ];
+      includes = [ "*.hs" ];
+    };
+  };
+}

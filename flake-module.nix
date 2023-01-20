f6fb7a360544ee36fb5271bd0a2f773811dc29fa
Jörg Thalheim <joerg@thalheim.io>
2023-01-20 09:52:44 +0100

    flake-module: enable treefmt as default formatter for the flake

--- flake-module.nix
+++ flake-module.nix
@@ -16,22 +16,25 @@ in
 
             Use `config.treefmt.build.wrapper` to get access to the resulting treefmt
             package based on this configuration.
-            
-            The following sets treefmt up as the default formatter 
-            used by the `nix fmt` command: 
-            
-            ```
-            perSystem = { config, ... }: {
-              formatter = config.treefmt.build.wrapper;
-            };
-            ```
+
+            By default treefmt-nix will set the formatter.<system> attribute of the flake,
+            used by the `nix fmt` command.
           '';
           type = types.submoduleWith {
-            modules = (import ./.).all-modules pkgs;
+            modules = (import ./.).all-modules pkgs ++ [{
+              options.flakeFormatter = lib.mkOption {
+                type = types.bool;
+                default = true;
+                description = lib.mdDoc ''
+                  Enables `treefmt` the default formatter used by the `nix fmt` command
+                '';
+              };
+            }];
           };
         };
         config = {
           checks.treefmt = config.treefmt.build.check self;
+          formatter = lib.mkIf config.treefmt.flakeFormatter (lib.mkDefault config.treefmt.build.wrapper);
         };
       });
   };

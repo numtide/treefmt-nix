d5ed9a1e6793f99b2e179d5dec9639e48ef22db7
Matthieu Coudron <teto@users.noreply.github.com>
2023-01-23 11:33:22 +0100

    feat: add programs.hlint (#33)
    style checker for haskell

---
+++ programs/hlint.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.programs.hlint;
+in
+{
+  options.programs.hlint = {
+    enable = lib.mkEnableOption "hlint";
+    package = lib.mkPackageOption pkgs "hlint" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.hlint = {
+      command = cfg.package;
+      options = [ "-j" ];
+      includes = [ "*.hs" ];
+    };
+  };
+}
+

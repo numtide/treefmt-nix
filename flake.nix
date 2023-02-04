e70e373bc5bc016ecef45c44bbbfb0ae28916391
zimbatm <zimbatm@zimbatm.com>
2023-02-04 01:15:18 +0100

    treefmt treefmt
    Exercise treefmt-nix from this project

--- flake.nix
+++ flake.nix
@@ -6,7 +6,16 @@
   outputs = { self, nixpkgs }:
     let
       lib = nixpkgs.lib;
-      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+      forAllSystems = lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      treefmtEval = forAllSystems (system:
+        self.lib.evalModule nixpkgs.legacyPackages.${system} ./treefmt.nix
+      );
     in
     {
       lib = import ./.;
@@ -14,17 +23,17 @@
       flakeModule = ./flake-module.nix;
 
       formatter = forAllSystems (system:
-        import ./treefmt.nix {
-          pkgs = nixpkgs.legacyPackages.${system};
-          treefmt-nix = self.lib;
-        }
+        treefmtEval.${system}.config.build.wrapper
       );
 
-      checks = forAllSystems (system:
-        import ./tests {
-          pkgs = nixpkgs.legacyPackages.${system};
-          treefmt-nix = self.lib;
-        }
-      );
+      checks = forAllSystems
+        (system:
+          (import ./tests {
+            pkgs = nixpkgs.legacyPackages.${system};
+            treefmt-nix = self.lib;
+          }) // {
+            formatting = treefmtEval.${system}.config.build.check self;
+          }
+        );
     };
 }
--- treefmt.nix
+++ treefmt.nix
@@ -1,5 +1,4 @@
-{ pkgs, treefmt-nix }:
-treefmt-nix.mkWrapper pkgs {
+{
   projectRootFile = "treefmt.nix";
   programs.nixpkgs-fmt.enable = true;
 }

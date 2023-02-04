33675fecb31c9179d2ae2c1aaceeaf255ae9282f
zimbatm <zimbatm@zimbatm.com>
2023-02-04 01:15:18 +0100

    treefmt treefmt
    Exercise treefmt-nix from this project

--- flake.nix
+++ flake.nix
@@ -1,8 +1,30 @@
 {
   description = "treefmt nix configuration modules";
 
-  outputs = { self }: {
-    lib = import ./.;
-    flakeModule = ./flake-module.nix;
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      lib = import ./.;
+
+      flakeModule = ./flake-module.nix;
+
+      formatter = forAllSystems (system:
+        import ./treefmt.nix {
+          pkgs = nixpkgs.legacyPackages.${system};
+          treefmt-nix = self.lib;
+        }
+      );
+
+      checks = forAllSystems (system:
+        import ./tests {
+          pkgs = nixpkgs.legacyPackages.${system};
+          treefmt-nix = self.lib;
+        }
+      );
+    };
 }

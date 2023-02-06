b73fff3e7cdeaf167f40478ddbfb5b5edf2e8e5c
Rashid Al Muhairi <r.muhairi@pm.me>
2023-02-06 17:17:40 +0400

    ocamlformat: Modify package option to support version detection

--- programs/ocamlformat.nix
+++ programs/ocamlformat.nix
@@ -4,17 +4,38 @@
 , ...
 }:
 let
+  inherit (lib) types;
+
   cfg = config.programs.ocamlformat;
+
+  detectVersion = configFile:
+    let
+      optionValue = list:
+        assert lib.assertMsg (list != [ ]) "treefmt/ocamlformat: Unable to find version in the config file";
+        lib.elemAt list (lib.length list - 1);
+    in
+    builtins.getAttr "ocamlformat_${
+      builtins.replaceStrings ["."] ["_"]
+      (optionValue (lib.findFirst (option: builtins.head option == "version") []
+          (builtins.map (n: lib.splitString "=" n) (lib.splitString "\n" (builtins.readFile configFile)))))
+    }"
+      pkgs;
 in
 {
   options.programs.ocamlformat = {
     enable = lib.mkEnableOption "ocamlformat";
-    package = lib.mkPackageOption pkgs "ocamlformat" { };
+    package = lib.mkOption {
+      type = types.either types.path types.package;
+      default = pkgs.ocamlformat;
+    };
   };
 
   config = lib.mkIf cfg.enable {
     settings.formatter.ocamlformat = {
-      command = cfg.package;
+      command =
+        if lib.isPath cfg.package
+        then detectVersion cfg.package
+        else cfg.package;
       options = [ "-i" ];
       includes = [ "*.ml" "*.mli" ];
     };

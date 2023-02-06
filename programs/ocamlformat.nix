a763fb087f4b91623a87c70bf75d030238619477
Rashid Al Muhairi <r.muhairi@pm.me>
2023-02-07 03:38:43 +0400

    ocamlformat: Format the module and modify the error message for clarity

--- programs/ocamlformat.nix
+++ programs/ocamlformat.nix
@@ -1,39 +1,40 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}: let
+{ lib
+, pkgs
+, config
+, ...
+}:
+let
   inherit (lib) types;
 
   cfg = config.programs.ocamlformat;
 
-  detectVersion = configFile: pkgSet: let
-    optionValue = list:
-      assert lib.assertMsg (list != []) "treefmt/ocamlformat: Unable to find version in the config file";
+  detectVersion = configFile: pkgSet:
+    let
+      optionValue = list:
+        assert lib.assertMsg (list != [ ]) "treefmt-nix: Unable to detect ocamlformat version from file";
         lib.elemAt list (lib.length list - 1);
-  in
+    in
     builtins.getAttr "ocamlformat_${
       builtins.replaceStrings ["."] ["_"]
       (optionValue (lib.findFirst (option: builtins.head option == "version") []
           (builtins.map (n: lib.splitString "=" n) (lib.splitString "\n" (builtins.readFile configFile)))))
     }"
-    pkgSet;
-in {
+      pkgSet;
+in
+{
   options.programs.ocamlformat = {
     enable = lib.mkEnableOption "ocamlformat";
     package = lib.mkOption {
       type = types.oneOf [
         types.path
         types.package
-
         (types.submodule {
           options = {
             pkgs = lib.mkOption {
               type = types.lazyAttrsOf types.raw;
               default = pkgs;
             };
-            path = lib.mkOption {type = types.path;};
+            path = lib.mkOption { type = types.path; };
           };
         })
       ];
@@ -47,8 +48,8 @@ in {
         if lib.isDerivation cfg.package
         then cfg.package
         else detectVersion cfg.package.path cfg.package.pkgs;
-      options = ["-i"];
-      includes = ["*.ml" "*.mli"];
+      options = [ "-i" ];
+      includes = [ "*.ml" "*.mli" ];
     };
   };
 }

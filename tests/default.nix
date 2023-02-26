c48e39d651ff14c0b01f629d4e5695e02396ff78
zimbatm <zimbatm@zimbatm.com>
2023-02-26 14:17:46 +0100

    bors.toml: sync with the checks
    Check if the bors.toml is out of sync

--- tests/default.nix
+++ tests/default.nix
@@ -6,19 +6,44 @@ let
     treefmt-nix.mkConfigFile pkgs {
       programs.${name}.enable = true;
     };
-in
-{
-  testEmptyConfig = treefmt-nix.mkConfigFile pkgs { };
 
-  testWrapper = treefmt-nix.mkWrapper pkgs {
-    projectRootFile = "flake.nix";
-  };
-} // (
-  pkgs.lib.listToAttrs (map
-    (name: {
-      name = name;
-      value = toConfig name;
-    })
-    treefmt-nix.programs.names
-  )
-)
+  borsToml =
+    let
+      checks = map (name: ''  "check ${name} [x86_64-linux]"'') (lib.attrNames self);
+    in
+    pkgs.writeText "bors.toml" ''
+      # Generated with ./bors.toml.sh
+      cut_body_after = "" # don't include text from the PR body in the merge commit message
+      status = [
+        "Evaluate flake.nix",
+        ${lib.concatStringsSep ",\n  " checks},
+      ]
+    '';
+
+  self = {
+    testEmptyConfig = treefmt-nix.mkConfigFile pkgs { };
+
+    testWrapper = treefmt-nix.mkWrapper pkgs {
+      projectRootFile = "flake.nix";
+    };
+
+    # Check if the bors.toml needs to be updated
+    testBorsToml = pkgs.runCommand
+      "test-bors-toml"
+      {
+        passthru.borsToml = borsToml;
+      } ''
+      diff ${../bors.toml} ${borsToml}
+      touch $out
+    '';
+  } // (
+    pkgs.lib.listToAttrs (map
+      (name: {
+        name = name;
+        value = toConfig name;
+      })
+      treefmt-nix.programs.names
+    )
+  );
+in
+self

fa92ec7d23abddb61d00ef86e8325835b7d7a95b
zimbatm <zimbatm@zimbatm.com>
2023-02-26 13:30:23 +0100

    tests: split generation for each program
    This makes it easier to parallelize and identify which program failed.

--- tests/default.nix
+++ tests/default.nix
@@ -1,24 +1,24 @@
 { pkgs, treefmt-nix, ... }:
 let
   inherit (pkgs) lib;
+
+  toConfig = name:
+    treefmt-nix.mkConfigFile pkgs {
+      programs.${name}.enable = true;
+    };
 in
 {
-  testConfigGeneration = treefmt-nix.mkConfigFile
-    pkgs
-    {
-      programs = lib.listToAttrs
-        (map
-          (name: {
-            name = name;
-            value = { enable = true; };
-          })
-          treefmt-nix.programs.names
-        );
-    };
-
   testEmptyConfig = treefmt-nix.mkConfigFile pkgs { };
 
   testWrapper = treefmt-nix.mkWrapper pkgs {
     projectRootFile = "flake.nix";
   };
-}
+} // (
+  pkgs.lib.listToAttrs (map
+    (name: {
+      name = name;
+      value = toConfig name;
+    })
+    treefmt-nix.programs.names
+  )
+)

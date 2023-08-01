de03b21f6fe4770166066e84969451e55211b760
Greg Pfeil <greg@technomadic.org>
2023-08-01 01:21:15 -0600

    Allow indent_size to be unset for shfmt (#97)
    * Allow indent_size to be unset for shfmt
    
    So the defaults don’t get in the way of reading the config file.
    
    * Revert the breaking change
    
    Also add some explanation and simplify a conditional.

--- programs/shfmt.nix
+++ programs/shfmt.nix
@@ -7,11 +7,13 @@ in
     enable = lib.mkEnableOption "shfmt";
     package = lib.mkPackageOption pkgs "shfmt" { };
     indent_size = lib.mkOption {
-      type = lib.types.int;
+      type = lib.types.nullOr lib.types.int;
       default = 2;
       example = 4;
       description = lib.mdDoc ''
-        Sets the number of spaces to be used in indentation. Uses tabs if set to zero.
+        Sets the number of spaces to be used in indentation. Uses tabs if set to
+        zero. If this is null, then [.editorconfig will be used to configure
+        shfmt](https://github.com/patrickvane/shfmt#description).
       '';
     };
   };
@@ -19,7 +21,10 @@ in
   config = lib.mkIf cfg.enable {
     settings.formatter.shfmt = {
       command = cfg.package;
-      options = [ "-i" (toString cfg.indent_size) "-s" "-w" ];
+      options =
+        (lib.optionals (!isNull cfg.indent_size)
+          [ "-i" (toString cfg.indent_size) ])
+        ++ [ "-s" "-w" ];
       includes = [ "*.sh" ];
     };
   };

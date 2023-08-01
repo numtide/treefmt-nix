3f2715671d4fd622869d2dcee8fc2d65f1314d98
Greg Pfeil <greg@technomadic.org>
2023-07-31 22:35:32 -0600

    Revert the breaking change
    Also add some explanation and simplify a conditional.

--- programs/shfmt.nix
+++ programs/shfmt.nix
@@ -8,10 +8,12 @@ in
     package = lib.mkPackageOption pkgs "shfmt" { };
     indent_size = lib.mkOption {
       type = lib.types.nullOr lib.types.int;
-      default = null;
+      default = 2;
       example = 4;
       description = lib.mdDoc ''
-        Sets the number of spaces to be used in indentation. Uses tabs if set to zero.
+        Sets the number of spaces to be used in indentation. Uses tabs if set to
+        zero. If this is null, then [.editorconfig will be used to configure
+        shfmt](https://github.com/patrickvane/shfmt#description).
       '';
     };
   };
@@ -20,9 +22,8 @@ in
     settings.formatter.shfmt = {
       command = cfg.package;
       options =
-        (if isNull cfg.indent_size
-        then [ ]
-        else [ "-i" (toString cfg.indent_size) ])
+        (lib.optionals (!isNull cfg.indent_size)
+          [ "-i" (toString cfg.indent_size) ])
         ++ [ "-s" "-w" ];
       includes = [ "*.sh" ];
     };

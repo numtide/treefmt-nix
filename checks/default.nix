6833ad724ed1a4bba59917a71b72bbb39dced71b
zimbatm <zimbatm@zimbatm.com>
2023-08-01 09:19:24 +0200

    remove bors
    Unfortunately Bors is not supported anymore.

--- checks/default.nix
+++ checks/default.nix
@@ -14,19 +14,6 @@ let
     treefmt-nix.programs.names
   );
 
-  borsToml =
-    let
-      checks = map (name: ''  "check ${name} [x86_64-linux]"'') (lib.attrNames self);
-    in
-    pkgs.writeText "bors.toml" ''
-      # Generated with ./bors.toml.sh
-      cut_body_after = "" # don't include text from the PR body in the merge commit message
-      status = [
-        "Evaluate flake.nix",
-        ${join ",\n  " checks},
-      ]
-    '';
-
   examples =
     let
       configs = lib.mapAttrs
@@ -64,21 +51,6 @@ let
     simple-wrapper = treefmt-nix.mkWrapper pkgs {
       projectRootFile = "flake.nix";
     };
-
-    # Check if the bors.toml needs to be updated
-    bors-toml = pkgs.runCommand
-      "test-bors-toml"
-      {
-        passthru.borsToml = borsToml;
-      }
-      ''
-        if ! diff ${../bors.toml} ${borsToml}; then
-          echo "The generated ./bors.toml is out of sync"
-          echo "Run ./bors.toml.sh to fix the issue"
-          exit 1
-        fi
-        touch $out
-      '';
 
     # Check if the examples folder needs to be updated
     examples = pkgs.runCommand
--- examples.sh
+++ examples.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Used to generate the bors.toml file
+# Used to generate the examples/ folder
 set -euo pipefail
 
 cd "$(dirname "$0")"
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 # Get the system tuple for later
 system=$(nix eval --raw --impure --expr 'builtins.currentSystem')
 
-# Generate the bors.toml from the config
+# Generate the examples from the config
 nix build ".#checks.$system.examples.passthru.examples"
 
 # Copy

7433633442a33ee8f7b5fb6951f5fdb8e3c589d6
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2023-08-10 17:57:59 +0000

    Add leptosfmt

--- README.md
+++ README.md
@@ -185,6 +185,7 @@ This repo contains a top-level `default.nix` that returns the library helper fun
 * hclfmt
 * hlint
 * isort
+* leptosfmt
 * mdformat
 * mdsh
 * mix-format
---
+++ programs/leptosfmt.nix
@@ -0,0 +1,17 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.programs.leptosfmt;
+in
+{
+  options.programs.leptosfmt = {
+    enable = lib.mkEnableOption "leptosfmt";
+    package = lib.mkPackageOption pkgs "leptosfmt" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.leptosfmt = {
+      command = "${cfg.package}/bin/leptosfmt";
+      includes = [ "*.rs" ];
+    };
+  };
+}

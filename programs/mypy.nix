64fd4d89bfbb0d414323e0d613cc7ceb3c7c3637
Jörg Thalheim <joerg@thalheim.io>
2023-08-12 14:33:36 +0200

    mypy: add support for non-module type checks

--- programs/mypy.nix
+++ programs/mypy.nix
@@ -32,6 +32,12 @@
                 example = [ "mymodule" "tests" ];
                 description = "Modules to check";
               };
+              files = lib.mkOption {
+                type = lib.types.listOf lib.types.str;
+                default = [ ];
+                example = [ "./**/tasks.py" ];
+                description = "Single files to check. Can be globs";
+              };
             };
           }
         ));
@@ -51,16 +57,18 @@
     settings.formatter = lib.mapAttrs'
       (name: cfg:
         lib.nameValuePair "mypy-${name}" {
-          command = "sh";
+          command = pkgs.bash;
           options = [
             "-eucx"
             ''
+              # to allow recursive globbing
+              shopt -s globstar
               cd "${cfg.directory}"
               export PYTHONPATH="${pkgs.python3.pkgs.makePythonPath cfg.extraPythonPackages}"
-              ${lib.getExe pkgs.mypy} ${lib.escapeShellArgs cfg.options} ${lib.escapeShellArgs cfg.modules}
+              ${lib.getExe pkgs.mypy} ${lib.escapeShellArgs cfg.options} ${lib.escapeShellArgs cfg.modules} ${builtins.toString cfg.files}
             ''
           ];
-          includes = builtins.map (module: "${cfg.directory}/${module}/**/*.py") cfg.modules;
+          includes = (builtins.map (module: "${cfg.directory}/${module}/**/*.py") cfg.modules) ++ cfg.files;
         })
       config.programs.mypy.directories;
   };

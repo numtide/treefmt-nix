bfef74a0092aa5d281b1dee00c619d5b2d8647be
Sridhar Ratnakumar <srid@srid.ca>
2023-08-13 10:54:07 -0400

    leptosfmt: Avoid writing to file if unchanged

--- programs/leptosfmt.nix
+++ programs/leptosfmt.nix
@@ -1,6 +1,19 @@
 { lib, pkgs, config, ... }:
 let
   cfg = config.programs.leptosfmt;
+  # Run the given program with the given argument (filepath), while expecting
+  # the program to *always* write back to the filepath. The program takes
+  # --stdin for alternative input.
+  #
+  # The resultant script behaves the same except for avoiding writing to file if
+  # the output hasn't changed.
+  modify-unless-unchanged = pkgs.writeShellScriptBin "modify-unless-unchanged"
+    ''
+      PROG=$1
+      FILEPATH=$2
+      OUTPUT=$($PROG --stdin < "$FILEPATH")
+      echo "$OUTPUT" | diff -q "$FILEPATH" - > /dev/null || echo "$OUTPUT" > "$FILEPATH"
+    '';
 in
 {
   options.programs.leptosfmt = {
@@ -12,10 +25,17 @@ in
     settings.formatter.leptosfmt = {
       command = pkgs.writeShellApplication {
         name = "leptosfmt-forall";
-        runtimeInputs = [ cfg.package ];
+        runtimeInputs = [
+          cfg.package
+          modify-unless-unchanged
+        ];
         text = ''
+          # leptosfmt doesn't take multiple arguments yet
+          # https://github.com/bram209/leptosfmt/issues/63
           for file in "$@"; do
-            leptosfmt "$file"
+            # Use the modify-unless-unchanged hack until this is implemented:
+            # https://github.com/bram209/leptosfmt/issues/64
+            modify-unless-unchanged leptosfmt "$file"
           done
         '';
       };

a127eeeac43555d75080f3d19dac40d8f76a85c6
Jörg Thalheim <Mic92@users.noreply.github.com>
2023-08-20 10:24:06 +0100

    mypy: add options parameter (#110)
    * mypy: add options parameter
    
    Since we are in a shell script we cannot pass flags in a different way
    
    * mypy: add support for non-module type checks
    
    ---------
    
    Co-authored-by: Jonas Chevalier <zimbatm@zimbatm.com>

--- programs/mypy.nix
+++ programs/mypy.nix
@@ -20,11 +20,23 @@
                 example = lib.literalMD ''[ pkgs.python3.pkgs.requests ]'';
                 description = "Extra packages to add to PYTHONPATH";
               };
+              options = lib.mkOption {
+                type = lib.types.listOf lib.types.str;
+                default = [ ];
+                example = [ "--ignore-missing-imports" ];
+                description = "Options to pass to mypy";
+              };
               modules = lib.mkOption {
                 type = lib.types.listOf lib.types.str;
                 default = [ "." ];
                 example = [ "mymodule" "tests" ];
                 description = "Modules to check";
+              };
+              files = lib.mkOption {
+                type = lib.types.listOf lib.types.str;
+                default = [ ];
+                example = [ "./**/tasks.py" ];
+                description = "Single files to check. Can be globs";
               };
             };
           }
@@ -45,16 +57,18 @@
     settings.formatter = lib.mapAttrs'
       (name: cfg:
         lib.nameValuePair "mypy-${name}" {
-          command = "sh";
+          command = pkgs.bash;
           options = [
             "-eucx"
             ''
+              # to allow recursive globbing
+              shopt -s globstar
               cd "${cfg.directory}"
               export PYTHONPATH="${pkgs.python3.pkgs.makePythonPath cfg.extraPythonPackages}"
-              ${lib.getExe pkgs.mypy} ${lib.escapeShellArgs cfg.modules}
+              ${lib.getExe pkgs.mypy} ${lib.escapeShellArgs cfg.options} ${lib.escapeShellArgs cfg.modules} ${builtins.toString cfg.files}
             ''
           ];
-          includes = builtins.map (module: "${cfg.directory}/${module}/**/*.py") cfg.modules;
+          includes = (builtins.map (module: "${cfg.directory}/${module}/**/*.py") cfg.modules) ++ cfg.files;
         })
       config.programs.mypy.directories;
   };

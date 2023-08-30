733ea34c0d82644bc422d5843ba0b5155ecd0026
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2023-08-30 21:05:39 +0100

    feat: init protolint

---
+++ programs/protolint.nix
@@ -0,0 +1,21 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.programs.protolint;
+in
+{
+  options.programs.protolint = {
+    enable = lib.mkEnableOption "protolint";
+    package = lib.mkPackageOption pkgs "protolint" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.protolint = {
+      command = cfg.package;
+      options = [
+        "lint"
+        "-fix"
+      ];
+      includes = [ "*.proto" ];
+    };
+  };
+}

720bd006d855b08e60664e4683ccddb7a9ff614a
Kasper Kondzielski <kghost0@gmail.com>
2023-09-27 15:55:46 +0200

    Add ktfmt (#124)
    Co-authored-by: ghostbuster91 <ghostbuster91@users.noreply.github.com>

---
+++ programs/ktfmt.nix
@@ -0,0 +1,20 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.programs.ktfmt;
+in
+{
+  options.programs.ktfmt = {
+    enable = lib.mkEnableOption "ktfmt";
+    package = lib.mkPackageOption pkgs "ktfmt" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.ktfmt = {
+      command = cfg.package;
+      includes = [
+        "*.kt"
+        "*.kts"
+      ];
+    };
+  };
+}

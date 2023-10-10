bec5981c92088431570dfbb7598301963ab763e5
gkze <george.kontridze@gmail.com>
2023-10-10 12:33:08 -0700

    Add support for `nickel format`

---
+++ programs/nickel.nix
@@ -0,0 +1,25 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.programs.nickel;
+
+  wrapper = pkgs.writeShellScriptBin "nickelfmt" ''
+    set -euo pipefail
+
+    for file in "$@"; do
+      ${cfg.package}/bin/nickel format --in-place --file "$file"
+    done
+  '';
+in
+{
+  options.programs.nickel = {
+    enable = lib.mkEnableOption "nickel";
+    package = lib.mkPackageOption pkgs "nickel" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.nickel = {
+      command = wrapper // { meta = config.package.meta // wrapper.meta; };
+      includes = [ "*.ncl" "*.nickel" ];
+    };
+  };
+}

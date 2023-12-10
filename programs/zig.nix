bc98fe4a197acc5aa5d0fa7fa4ae0411e70e3495
Callum Iddon <19756675+CallumIddon@users.noreply.github.com>
2023-12-10 20:16:40 +1300

    add zig fmt

---
+++ programs/zig.nix
@@ -0,0 +1,18 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.programs.zig;
+in
+{
+  options.programs.zig = {
+    enable = lib.mkEnableOption "zig";
+    package = lib.mkPackageOption pkgs "zig" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.zig = {
+      command = cfg.package;
+      options = [ "fmt" ];
+      includes = [ "*.zig" ];
+    };
+  };
+}

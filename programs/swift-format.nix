834d4f2e5bfbbff65c4fa2e7129599e2657c025e
Callum Iddon <19756675+CallumIddon@users.noreply.github.com>
2023-12-19 17:28:31 +1300

    add swift-format

---
+++ programs/swift-format.nix
@@ -0,0 +1,18 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.programs.swift-format;
+in
+{
+  options.programs.swift-format = {
+    enable = lib.mkEnableOption "swift-format";
+    package = lib.mkPackageOption pkgs "swift-format" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.swift-format = {
+      command = cfg.package;
+      options = [ "-i" ];
+      includes = [ "*.swift" ];
+    };
+  };
+}

3ba7dd66d267d8e30daefcf736eabc4f7f5dd98b
Johannes Gerold <git+commit@jgero.me>
2024-02-25 20:56:12 +0100

    feat(programs): add templ

---
+++ programs/templ.nix
@@ -0,0 +1,18 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.programs.templ;
+in
+{
+  options.programs.templ = {
+    enable = lib.mkEnableOption "templ";
+    package = lib.mkPackageOption pkgs "templ" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.templ = {
+      command = "${cfg.package}/bin/templ";
+      options = [ "fmt" ];
+      includes = [ "*.templ" ];
+    };
+  };
+}

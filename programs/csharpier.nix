7ee5aaac63c30d3c97a8c56efe89f3b2aa9ae564
Thomas Laich <37534906+thomaslaich@users.noreply.github.com>
2024-03-18 17:58:23 +0100

    csharpier extensions (#170)
    add includes and excludes options for csharpier

--- programs/csharpier.nix
+++ programs/csharpier.nix
@@ -1,13 +1,28 @@
 { lib, pkgs, config, ... }:
-let cfg = config.programs.csharpier;
-in {
+let
+  cfg = config.programs.csharpier;
+  inherit (lib) mkEnableOption mkOption mkPackageOption mkIf types;
+in
+{
   options.programs.csharpier = {
-    enable = lib.mkEnableOption "csharpier";
-    package = lib.mkPackageOption pkgs "csharpier" { };
-    dotnet-sdk = lib.mkPackageOption pkgs "dotnet-sdk" { };
+    enable = mkEnableOption "csharpier";
+    package = mkPackageOption pkgs "csharpier" { };
+    dotnet-sdk = mkPackageOption pkgs "dotnet-sdk" { };
+
+    includes = mkOption {
+      description = "Path / file patterns to include for CSharpier";
+      type = types.listOf types.str;
+      default = [ "*.cs" ];
+    };
+
+    excludes = mkOption {
+      description = "Path / file patterns to exclude for CSharpier";
+      type = types.listOf types.str;
+      default = [ ];
+    };
   };
 
-  config = lib.mkIf cfg.enable {
+  config = mkIf cfg.enable {
     settings.formatter.csharpier = {
       command = pkgs.writeShellApplication {
         name = "dotnet-csharpier";
@@ -16,7 +31,8 @@ in {
           dotnet-csharpier "$@"
         '';
       };
-      includes = [ "*.cs" ];
+      includes = cfg.includes;
+      excludes = cfg.excludes;
     };
   };
 }

1d347849c450825efb5f89421e62b7d67cf7c018
RTUnreal <unreal@rtinf.net>
2024-03-27 09:13:48 +0100

    php-cs-fixer: add configFile option

--- programs/php-cs-fixer.nix
+++ programs/php-cs-fixer.nix
@@ -1,17 +1,24 @@
 { lib, pkgs, config, ... }:
 let
+  inherit (lib) types;
   cfg = config.programs.php-cs-fixer;
 in
 {
   options.programs.php-cs-fixer = {
     enable = lib.mkEnableOption "php-cs-fixer";
     package = lib.mkPackageOption pkgs.phpPackages "php-cs-fixer" { };
+    configFile = lib.mkOption {
+      description = "Path to php-cs-fixer config file.";
+      type = types.oneOf [ types.str types.path ];
+      default = "./.php-cs-fixer.php";
+      example = "./.php-cs-fixer.dist.php";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     settings.formatter.php-cs-fixer = {
       command = "${cfg.package}/bin/php-cs-fixer";
-      options = [ "fix" ];
+      options = [ "fix" "--config" "${cfg.configFile}" ];
       includes = [ "*.php" ];
     };
   };

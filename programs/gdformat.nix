ace1ee52735055383bf674c46f50af374bbb54d6
haruki7049 <tontonkirikiri@gmail.com>
2024-03-30 19:37:11 +0900

    gdformat: Added gdformat

---
+++ programs/gdformat.nix
@@ -0,0 +1,17 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.programs.gdformat;
+in
+{
+  options.programs.gdformat = {
+    enable = lib.mkEnableOption "gdformat";
+    package = lib.mkPackageOption pkgs "gdtoolkit" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.gdformat = {
+      command = "${cfg.package}/bin/gdformat";
+      includes = [ "*.gd" ];
+    };
+  };
+}

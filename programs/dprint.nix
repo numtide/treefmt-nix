03b982b77df58d5974c61c6022085bafe780c1cf
Hoang Nguyen <folliekazetani@protonmail.com>
2024-05-31 19:02:28 +0000

    feat(dprint): allow custom options for external formatters (#180)
    by making the `settings` option a freeformType.
    
    Fixes #149.
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- programs/dprint.nix
+++ programs/dprint.nix
@@ -6,76 +6,82 @@ let
   cfg = config.programs.dprint;
   configFormat = pkgs.formats.json { };
 
-  # Configuration schema for dprint, which we generate dprint.json with.
   # Definition taken from:
   # https://raw.githubusercontent.com/dprint/dprint/5168db9ff0a9b5d42774f95edd58681fc67c9009/website/src/assets/schemas/v0.json
-  settingsSchema = {
-    incremental = mkOption {
-      description = "Whether to format files only when they change.";
-      type = types.nullOr types.bool;
-      example = true;
-      default = null;
-    };
-    extends = mkOption {
-      description = "Configurations to extend.";
-      type = types.nullOr (types.oneOf [ types.str (types.listOf types.str) ]);
-      example = "https://dprint.dev/path/to/config/file.v1.json";
-      default = null;
-    };
-    lineWidth = mkOption {
-      description = ''
-        The width of a line the printer will try to stay under. Note that the
-        printer may exceed this width in certain cases.
-      '';
-      type = types.nullOr types.int;
-      example = 80;
-      default = null;
-    };
-    indentWidth = mkOption {
-      description = "The number of characters for an indent";
-      type = types.nullOr types.int;
-      example = 2;
-      default = null;
-    };
-    useTabs = mkOption {
-      description = ''
-        Whether to use tabs (true) or spaces (false) for indentation.
-      '';
-      type = types.nullOr types.bool;
-      example = true;
-      default = null;
-    };
-    newLineKind = mkOption {
-      description = ''
-        The kind of newline to use (one of: auto, crlf, lf, system).
-      '';
-      type = types.nullOr types.str;
-      example = "auto";
-      default = null;
-    };
-    includes = mkOption {
-      description = "Array of patterns (globs) to use to find files to format.";
-      type = types.nullOr (types.listOf types.str);
-      example = [ "**/*.{ts,tsx,js,jsx,mjs,json,md}" ];
-      default = null;
-    };
-    excludes = mkOption {
-      description = ''
-        Array of patterns (globs) to exclude files or directories to format.
-      '';
-      type = types.nullOr (types.listOf types.str);
-      example = [ "**/node_modules" "**/*-lock.json" ];
-      default = null;
-    };
-    plugins = mkOption {
-      description = "Array of plugin URLs to format files.";
-      type = types.nullOr (types.listOf types.str);
-      example = [
-        "https://plugins.dprint.dev/json-0.17.2.wasm"
-        "https://plugins.dprint.dev/markdown-0.15.2.wasm"
-        "https://plugins.dprint.dev/typescript-0.84.4.wasm"
-      ];
-      default = null;
+  settingsSchema = mkOption {
+    default = { };
+    description = "Configuration to generate dprint.json with";
+    type = types.submodule {
+      freeformType = configFormat.type;
+      options = {
+        incremental = mkOption {
+          description = "Whether to format files only when they change.";
+          type = types.nullOr types.bool;
+          example = true;
+          default = null;
+        };
+        extends = mkOption {
+          description = "Configurations to extend.";
+          type = types.nullOr (types.oneOf [ types.str (types.listOf types.str) ]);
+          example = "https://dprint.dev/path/to/config/file.v1.json";
+          default = null;
+        };
+        lineWidth = mkOption {
+          description = ''
+            The width of a line the printer will try to stay under. Note that the
+            printer may exceed this width in certain cases.
+          '';
+          type = types.nullOr types.int;
+          example = 80;
+          default = null;
+        };
+        indentWidth = mkOption {
+          description = "The number of characters for an indent";
+          type = types.nullOr types.int;
+          example = 2;
+          default = null;
+        };
+        useTabs = mkOption {
+          description = ''
+            Whether to use tabs (true) or spaces (false) for indentation.
+          '';
+          type = types.nullOr types.bool;
+          example = true;
+          default = null;
+        };
+        newLineKind = mkOption {
+          description = ''
+            The kind of newline to use (one of: auto, crlf, lf, system).
+          '';
+          type = types.nullOr types.str;
+          example = "auto";
+          default = null;
+        };
+        includes = mkOption {
+          description = "Array of patterns (globs) to use to find files to format.";
+          type = types.nullOr (types.listOf types.str);
+          example = [ "**/*.{ts,tsx,js,jsx,mjs,json,md}" ];
+          default = null;
+        };
+        excludes = mkOption {
+          description = ''
+            Array of patterns (globs) to exclude files or directories to format.
+          '';
+          type = types.nullOr (types.listOf types.str);
+          example = [ "**/node_modules" "**/*-lock.json" ];
+          default = null;
+        };
+        plugins = mkOption {
+          description = "Array of plugin URLs to format files.";
+          type = types.nullOr (types.listOf types.str);
+          example = [
+            "https://plugins.dprint.dev/json-0.17.2.wasm"
+            "https://plugins.dprint.dev/markdown-0.15.2.wasm"
+            "https://plugins.dprint.dev/typescript-0.84.4.wasm"
+          ];
+          default = null;
+        };
+      };
     };
   };
 

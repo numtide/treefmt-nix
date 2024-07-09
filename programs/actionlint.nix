8b63fe8cf7892c59b3df27cbcab4d5644035d72f
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-07-09 08:36:52 +0200

    add actionlint (#146)
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ programs/actionlint.nix
@@ -0,0 +1,17 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.programs.actionlint;
+in
+{
+  options.programs.actionlint = {
+    enable = lib.mkEnableOption "actionlint";
+    package = lib.mkPackageOption pkgs "actionlint" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.actionlint = {
+      command = cfg.package;
+      includes = [ ".github/workflows/*.yml" ];
+    };
+  };
+}

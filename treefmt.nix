c737821f24582d11de2dd121ac91bead4d254d05
Jörg Thalheim <joerg@thalheim.io>
2024-07-14 06:06:53 +0200

    enable formatter for all files

--- treefmt.nix
+++ treefmt.nix
@@ -1,5 +1,10 @@
-{
+{ pkgs, ... }: {
   projectRootFile = "treefmt.nix";
   programs.mdsh.enable = true;
+  programs.yamlfmt.enable = true;
+  programs.deno.enable = pkgs.hostPlatform.system != "riscv64-linux";
   programs.nixpkgs-fmt.enable = true;
+  programs.shfmt.enable = pkgs.hostPlatform.system != "riscv64-linux";
+  programs.shellcheck.enable = pkgs.hostPlatform.system != "riscv64-linux";
+  settings.global.excludes = [ "*.toml" ];
 }

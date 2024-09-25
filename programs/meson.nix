9ee50d37acbbc37fc51335d9bfd0f4c8d2d9867b
Christopher Smyth <18294397+RossSmyth@users.noreply.github.com>
2024-09-25 03:26:13 -0400

    meson: format all meson files (#242)

--- programs/meson.nix
+++ programs/meson.nix
@@ -7,7 +7,7 @@ let
   cfg = config.programs.meson;
 in
 {
-  meta.maintainers = [ ];
+  meta.maintainers = [ "RossSmyth" ];
 
   options.programs.meson = {
     enable = lib.mkEnableOption "meson";
@@ -22,6 +22,9 @@ in
         "meson.build"
         "meson.options"
         "meson_options.txt"
+        "*/meson.build"
+        "*/meson.options"
+        "*/meson_options.txt"
       ];
     };
   };

6ad2dc8239bfd1019ab80bb6883b1aa7796122c7
Jordan Williams <jordan@jwillikers.com>
2024-10-16 08:03:48 -0500

    nufmt: init
    Add nufmt for formatting Nushell files.

---
+++ programs/nufmt.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.programs.nufmt;
+in
+{
+  options.programs.nufmt = {
+    enable = lib.mkEnableOption "nufmt";
+    package = lib.mkPackageOption pkgs "nufmt" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.nufmt = {
+      command = "${cfg.package}/bin/nufmt";
+      includes = [
+        "*.nu"
+      ];
+    };
+  };
+}

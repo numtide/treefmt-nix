1358917adbb656df35110535081306ce43dfb357
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-10-27 14:14:09 +0100

    yamlfmt: add settings option
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- programs/yamlfmt.nix
+++ programs/yamlfmt.nix
@@ -1,6 +1,12 @@
-{ lib, pkgs, config, ... }:
+{ lib
+, pkgs
+, config
+, ...
+}:
 let
   cfg = config.programs.yamlfmt;
+
+  settingsFormat = pkgs.formats.yaml { };
 in
 {
   meta.maintainers = [ ];
@@ -8,12 +14,28 @@ in
   options.programs.yamlfmt = {
     enable = lib.mkEnableOption "yamlfmt";
     package = lib.mkPackageOption pkgs "yamlfmt" { };
+    settings = lib.mkOption {
+      type = lib.types.submodule { freeformType = settingsFormat.type; };
+      default = { };
+      description = ''
+        Configuration for Foo, see
+        <link xlink:href="https://github.com/google/yamlfmt/blob/main/docs/config-file.md"/>
+        for supported values.
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
     settings.formatter.yamlfmt = {
       command = cfg.package;
-      includes = [ "*.yaml" "*.yml" ];
+      includes = [
+        "*.yaml"
+        "*.yml"
+      ];
+      options = lib.optional
+        (
+          cfg.settings != { }
+        ) "-conf=${settingsFormat.generate "yamlfmt.conf" cfg.settings}";
     };
   };
 }

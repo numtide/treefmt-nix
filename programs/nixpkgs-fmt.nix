746901bb8dba96d154b66492a29f5db0693dbfcc
Sebastián Zavala Villagómez <sebaszv@proton.me>
2024-10-30 15:57:17 -0500

    nixpkgs-fmt: add `includes` and `excludes` options (#254)

--- programs/nixpkgs-fmt.nix
+++ programs/nixpkgs-fmt.nix
@@ -8,12 +8,26 @@ in
   options.programs.nixpkgs-fmt = {
     enable = lib.mkEnableOption "nixpkgs-fmt";
     package = lib.mkPackageOption pkgs "nixpkgs-fmt" { };
+
+    includes = lib.mkOption {
+      description = "Path/file patterns to include for nixpkgs-fmt";
+      type = lib.types.listOf lib.types.str;
+      default = [ "*.nix" ];
+    };
+    excludes = lib.mkOption {
+      description = "Path/file patterns to exclude for nixpkgs-fmt";
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+    };
   };
 
   config = lib.mkIf cfg.enable {
     settings.formatter.nixpkgs-fmt = {
       command = cfg.package;
-      includes = [ "*.nix" ];
+
+      inherit (cfg)
+        includes
+        excludes;
     };
   };
 }

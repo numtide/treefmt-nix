67a631bdee1638f200c24892226c740baff5ff2e
o-az <omaraziz.dev@proton.me>
2024-11-12 15:41:05 -0800

    feat: sqlfluff support

--- README.md
+++ README.md
@@ -293,6 +293,7 @@ functions.
 * scalafmt
 * shellcheck
 * shfmt
+* sqlfluff
 * statix
 * stylish-haskell
 * stylua
---
+++ sqlfluff.nix
@@ -0,0 +1,68 @@
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.programs.sqlfluff;
+
+  # https://github.com/sqlfluff/sqlfluff/blob/main/README.md#dialects-supported
+  dialects = [
+    "db2"
+    "ansi"
+    "soql"
+    "tsql"
+    "hive"
+    "trino"
+    "mysql"
+    "oracle"
+    "sqlite"
+    "duckdb"
+    "exasol"
+    "athena"
+    "mariadb"
+    "vertica"
+    "teradata"
+    "redshift"
+    "sparksql"
+    "bigquery"
+    "postgres"
+    "greenplum"
+    "snowflake"
+    "materializ"
+    "clickhouse"
+    "databricks"
+  ];
+in
+{
+  meta.maintainers = [ ];
+
+  options.programs.sqlfluff = {
+    enable = lib.mkEnableOption "sqlfluff";
+
+    package = lib.mkPackageOption pkgs "sqlfluff" { };
+
+    dialect = lib.mkOption {
+      description = "The sql dialect to use for formatting";
+      type = lib.types.enum dialects;
+      example = "sqlite";
+      default = null;
+    };
+    includes = lib.mkOption {
+      type = lib.types.nullOr (lib.types.listOf lib.types.str);
+      default = [ "*.sql" ];
+    };
+    excludes = lib.mkOption {
+      type = lib.types.nullOr (lib.types.listOf lib.types.str);
+      default = [ ];
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.sqlfluff = {
+      command = cfg.package;
+      options = [
+        "format"
+        "--dialect=${cfg.dialect}"
+      ];
+      includes = cfg.includes;
+      excludes = cfg.excludes;
+    };
+  };
+}

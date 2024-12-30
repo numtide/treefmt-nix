11b4444d3a678b7ea09b57a7aeaee3c09f4c0e21
Gabriel Nützi <gnuetzi@gmail.com>
2024-12-30 20:39:40 +0100

    feat: add 'goimports' formatter

---
+++ programs/goimports.nix
@@ -0,0 +1,25 @@
+{ lib
+, pkgs
+, config
+, ...
+}:
+let
+  cfg = config.programs.goimports;
+in
+{
+  meta.maintainers = [ "gabyx" ];
+
+  options.programs.goimports = {
+    enable = lib.mkEnableOption "goimports";
+    package = lib.mkPackageOption pkgs "gotools" { };
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.goimports = {
+      command = "${cfg.package}/bin/goimports";
+      options = [ "-w" ];
+      includes = [ "*.go" ];
+      excludes = [ "vendor/*" ];
+    };
+  };
+}

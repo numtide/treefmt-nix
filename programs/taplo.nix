e9f44d41f89d136409a6ca2af38545a5098eaad4
s3igo <85787242+s3igo@users.noreply.github.com>
2025-04-29 17:34:36 +0900

    taplo: add option to pass settings

--- programs/taplo.nix
+++ programs/taplo.nix
@@ -1,4 +1,32 @@
-{ mkFormatterModule, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  mkFormatterModule,
+  ...
+}:
+let
+  inherit (lib) mkOption types;
+
+  cfg = config.programs.taplo;
+  configFormat = pkgs.formats.toml { };
+  settingsSchema = mkOption {
+    description = "Configuration to generate taplo.toml with";
+    default = { };
+    type = types.submodule { freeformType = configFormat.type; };
+  };
+  settingsFile =
+    if cfg.settings != { } then
+      configFormat.generate "taplo.toml" (
+        cfg.settings
+        // {
+          include = cfg.includes;
+          exclude = cfg.excludes;
+        }
+      )
+    else
+      null;
+in
 {
   meta.maintainers = [ ];
 
@@ -9,4 +37,17 @@
       includes = [ "*.toml" ];
     })
   ];
+
+  options.programs.taplo = {
+    settings = settingsSchema;
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.taplo = {
+      options = lib.optionals (settingsFile != null) [
+        "--config"
+        (toString settingsFile)
+      ];
+    };
+  };
 }

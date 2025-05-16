a5b4bbb8d475b200d8ae7dc5626927960faf62c3
drew-dirac <drew@diracinc.com>
2025-05-16 11:08:54 -0400

    buf: doesn't support multiple file targets
    add to maintainers list

--- programs/buf.nix
+++ programs/buf.nix
@@ -1,17 +1,33 @@
-{ mkFormatterModule, ... }:
 {
-  meta.maintainers = [ ];
+  lib,
+  pkgs,
+  config,
+  mkFormatterModule,
+  ...
+}:
+let
+  cfg = config.programs.buf;
+in
+{
+  meta.maintainers = [
+    "drew-dirac"
+  ];
 
   imports = [
     (mkFormatterModule {
       name = "buf";
-      args = [
-        "format"
-        "-w"
-      ];
-      includes = [
-        "*.proto"
-      ];
+      includes = [ "*.proto" ];
     })
   ];
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.buf = {
+      # buf doesn't support multiple file targets
+      command = pkgs.writeShellScriptBin "buf-wrapper" ''
+        for file in "$@"; do
+          ${lib.getExe cfg.package} format -w "$file"
+        done
+      '';
+    };
+  };
 }

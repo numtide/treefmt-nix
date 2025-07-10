388d1056e8adf19453993029b8569c0b00a74a07
ncaq <ncaq@ncaq.net>
2025-07-10 14:44:29 +0900

    fix(cabal): cabal-fmt and cabal-gild discover module
    `cabal-fmt` and `cabal-gild` can discover module and autocomplete.
    ```cabal
    -- cabal-gild: discover ./src
    exposed-modules:
    ```
    The current system, treefmt can't detect change haskell module.
    I write to depend to haskell module file.

--- programs/cabal-fmt.nix
+++ programs/cabal-fmt.nix
@@ -1,4 +1,13 @@
-{ mkFormatterModule, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  mkFormatterModule,
+  ...
+}:
+let
+  cfg = config.programs.cabal-fmt;
+in
 {
   meta.maintainers = [ ];
 
@@ -10,7 +19,51 @@
         "cabal-fmt"
       ];
       args = [ "--inplace" ];
-      includes = [ "*.cabal" ];
+      includes = [
+        "*.cabal"
+        # Include Haskell source files to detect changes
+        # for cabal-fmt's module discovery feature
+        "*.chs"
+        "*.cpphs"
+        "*.gc"
+        "*.hs"
+        "*.hsc"
+        "*.hsig"
+        "*.lhs"
+        "*.lhsig"
+        "*.ly"
+        "*.x"
+        "*.y"
+      ];
     })
   ];
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.cabal-fmt = {
+      # Override command to filter out non-cabal files
+      command = pkgs.writeShellApplication {
+        name = "cabal-fmt-wrapper";
+        text = ''
+          cabal_files=()
+          for file in "$@"; do
+            # Only process .cabal files
+            case "$file" in
+              *.cabal)
+                cabal_files+=("$file")
+                ;;
+              *)
+                # Skip non-cabal files (e.g., .hs files)
+                ;;
+            esac
+          done
+
+          if [ ''${#cabal_files[@]} -gt 0 ]; then
+            ${lib.getExe cfg.package} --inplace "''${cabal_files[@]}"
+          fi
+        '';
+      };
+      # Clear args since we're handling them in the wrapper
+      options = lib.mkForce [ ];
+    };
+  };
 }

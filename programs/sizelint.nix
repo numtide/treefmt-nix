7d81f6fb2e19bf84f1c65135d1060d829fae2408
kenji <aks.kenji@protonmail.com>
2025-08-10 19:42:06 +0200

    sizelint: init at 0.1.3 (#401)
    * sizelint: init at 0.1.3
    
    * sizelint: add a-kenji as maintainer

---
+++ programs/sizelint.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  pkgs,
+  config,
+  mkFormatterModule,
+  ...
+}:
+let
+  inherit (lib) mkOption types;
+
+  cfg = config.programs.sizelint;
+  configFormat = pkgs.formats.toml { };
+  settingsSchema = mkOption {
+    description = "Configuration to generate sizelint.toml with";
+    default = { };
+    type = types.submodule { freeformType = configFormat.type; };
+  };
+  settingsFile =
+    if cfg.settings != { } then configFormat.generate "sizelint.toml" (cfg.settings) else null;
+in
+{
+  meta.maintainers = [ "a-kenji" ];
+
+  imports = [
+    (mkFormatterModule {
+      name = "sizelint";
+      args = [ "check" ];
+      includes = [ "*" ];
+    })
+  ];
+
+  options.programs.sizelint = {
+    settings = settingsSchema;
+    failOnWarn = lib.mkEnableOption "fail-on-warn";
+  };
+
+  config = lib.mkIf cfg.enable {
+    settings.formatter.sizelint = {
+      options =
+        lib.optionals (settingsFile != null) [
+          "--config"
+          (toString settingsFile)
+        ]
+        ++ lib.optional cfg.failOnWarn "--fail-on-warn";
+    };
+  };
+}

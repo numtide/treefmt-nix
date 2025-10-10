9e22722136fdd3fc9c37f9f37dfb12f598df69ba
shimeoki <shimeoki@gmail.com>
2025-10-10 20:06:51 +0300

    golines: add maxLength and tabLength options

--- programs/golines.nix
+++ programs/golines.nix
@@ -25,6 +25,26 @@ in
     })
   ];
 
+  options.programs.golines = {
+    maxLength = lib.mkOption {
+      type = with lib.types; nullOr int;
+      default = null;
+      example = 100;
+      description = ''
+        Target maximum line length (default: 100)
+      '';
+    };
+
+    tabLength = lib.mkOption {
+      type = with lib.types; nullOr int;
+      default = null;
+      example = 4;
+      description = ''
+        Length of a tab (default: 4).
+      '';
+    };
+  };
+
   config = lib.mkIf cfg.enable {
     settings.formatter.golines = {
       command = "${cfg.package}/bin/golines";

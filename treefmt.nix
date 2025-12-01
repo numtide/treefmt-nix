93ed7519ea5551dc530298b43db38c0a82a0d836
ryota2357 <noreply@ryota2357.com>
2025-12-01 13:35:54 +0900

    refactor: rename 'hostPlatform' to 'stdenv.hostPlatform'

--- treefmt.nix
+++ treefmt.nix
@@ -4,10 +4,10 @@
   settings.global.excludes = [ "*.toml" ];
 
   programs.deadnix.enable = true;
-  programs.deno.enable = pkgs.hostPlatform.system != "riscv64-linux";
+  programs.deno.enable = pkgs.stdenv.hostPlatform.system != "riscv64-linux";
   programs.mdsh.enable = true;
   programs.nixfmt.enable = true;
-  programs.shellcheck.enable = pkgs.hostPlatform.system != "riscv64-linux";
-  programs.shfmt.enable = pkgs.hostPlatform.system != "riscv64-linux";
+  programs.shellcheck.enable = pkgs.stdenv.hostPlatform.system != "riscv64-linux";
+  programs.shfmt.enable = pkgs.stdenv.hostPlatform.system != "riscv64-linux";
   programs.yamlfmt.enable = true;
 }

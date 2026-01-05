68a369e958065041e68b6cf7afa3028fbef80a11
Vidhan Bhatt <me@vidhan.io>
2026-01-05 02:16:31 -0500

    fix(xmllint): avoid always marking as changed

--- programs/xmllint.nix
+++ programs/xmllint.nix
@@ -33,8 +33,12 @@ in
       command = pkgs.writeShellApplication {
         name = "xmllint-wrapper";
         text = ''
+          temp=$(mktemp)
           for file in "$@"; do
-            ${lib.getExe' cfg.package "xmllint"} --format "$file" --output "$file"
+            ${lib.getExe' cfg.package "xmllint"} --format "$file" --output "$temp"
+            if ! cmp -s "$file" "$temp"; then
+              cp "$temp" "$file"
+            fi
           done
         '';
       };
